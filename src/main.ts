675f147b1bdfba0f4bbd85281e881fcccdb06384
sengokyu <sengokyu+gh@gmail.com>
2021-11-02 11:10:16 +0900

    Complete.

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { randomUUID } from "crypto";
+import { createWriteStream, WriteFileOptions } from "fs";
+import path from "path";
+import { from } from "rxjs";
+import { Stream } from "stream";
+
+const url =
+  "https://raw.githubusercontent.com/sengokyu/ex-axios-stream/main/bigfile/japanese-constitution.pdf";
+const dstPath = path.join(__dirname, randomUUID()) + ".pdf";
+
+from(axios.get<Stream>(url, { responseType: "stream" })).subscribe((resp) => {
+  const dst = createWriteStream(dstPath, "binary");
+  resp.data.pipe(dst);
+  console.log(`${dstPath} downloaded.`);
+});
